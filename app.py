53bed5a33745952ce0000023ad563a8a5b466294
Pedro Garcias <pedro.garcias@a3data.com.br>
2022-08-23 02:05:03 -0300

    first commit1

--- app.py
+++ app.py
@@ -41,46 +41,12 @@ ind = dict(zip(df_cut['Industries'].value_counts().index,
 df_cut['Industries'].value_counts().values))
 radio = st.radio("",['Todos','Visão industria'])
 if radio == 'Todos':
-    bt_gerar = st.button('Gerar')
-    if bt_gerar:
-        got_net = Network(height='1000px', width='100%', bgcolor='#222222', font_color='white')
-
-        # set the physics layout of the network
-        #got_net.barnes_hut(gravity=-80000, central_gravity=0.3, spring_length=250, spring_strength=0.001, damping=0.09, overlap=1)
-        got_net.force_atlas_2based(gravity= -1500, central_gravity=0.005, spring_length=100,spring_strength=0.08,damping=0.4,overlap=0.5)
-        #got_net.hrepulsion()
-        #got_net.repulsion()
-        got_net.inherit_edge_colors(True)
-        sources = df_cut['name']
-        targets = df_cut['Industries']
-        weights = df_cut['weight']
-        edge_data = zip(sources, targets, weights)
-
-        for key in ind.keys():
-            got_net.add_node(key, key, title=key,group=key, size=int(ind[key]))
-
-        for src in sources.unique():
-            got_net.add_node(src, src, title=src,size=1)
-
-        for e in edge_data:
-            src = e[0]
-            dst = e[1]
-            w = e[2]
-            got_net.add_edge(src, dst, value=1)
-
-        neighbor_map = got_net.get_adj_list()
-
-        #got_net.set_edge_smooth('curvedCW')
-        got_net.inherit_edge_colors(True)
-        #got_net.set_edge_smooth('continuous')
-        # add neighbor data to node hover data
-        #for node in got_net.nodes:
-        #    node['title'] += ' Neighbors:<br>' + '<br>'.join(neighbor_map[node['id']])
-            #node['value'] = node['size']
-        got_net.show_buttons(filter_=['physics'])
-        #got_net.toggle_stabilization(False)
-        #got_net.show('gameofthrones.html')
-        gerar_graph(got_net)
+    with open("graph.html", "rb") as file:
+         btn = st.download_button(
+                 label="Download HTML",
+                 data=file,
+                 file_name="graph.html"
+               )
 
 else:
     selected = st.selectbox('Selecionar categoria', ind.keys())
